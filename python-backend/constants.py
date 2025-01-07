23f5c9fb3e34505323122d96a607fafa4006eee0
YoussefMaklad <145257225+YoussefMaklad@users.noreply.github.com>
2025-01-07 14:44:52 +0200

    Update constants.py

--- python-backend/constants.py
+++ python-backend/constants.py
@@ -20,10 +20,11 @@ animals = {"cat", "dog", "bird", "horse", "sheep", "giraffe", "bear", "zebra", "
 emotion_buffer = []
 emotions = ["happy", "sad", "angry", "neutral"]
 
-model_dict = pickle.load(open('./gestures_training/classifier.p', 'rb'))
-model = model_dict['classifier']
-labels_dict = {0: 'Rotate', 1: 'Home', 2: 'Farm', 3: 'WildLife', 4: 'Select'}
+model_dict = pickle.load(open('./classifier-new.p', 'rb'))
+model = model_dict['classifier-new']
+labels_dict_kid = {0: 'Rotate', 1: 'Home', 2: 'Farm', 3: 'WildLife', 4: 'Select', 5: 'Back'}
+labels_dict_teacher = {0: 'HappyKids', 1: 'SadKids', 2: 'NeutralKids', 3: 'FearKids', 4: 'AngryKids', 5: "SurpriseStudents"}
+labels_dict = None
 
-KID_ID = None
 CSV_FILE = "gaze_coordinates.csv"
 gaze = GazeTracking()
